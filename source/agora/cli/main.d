8cc680f012283c5ed44872954809ac73d5390e31
MukeunKim <mukeunkim@gmail.com>
2019-08-28 19:06:39 +0900

    Implement parsing command line

--- source/agora/cli/main.d
+++ source/agora/cli/main.d
@@ -13,17 +13,47 @@
 
 module agora.cli.main;
 
+import agora.cli.CommandLine;
+
+import vibe.core.core;
+
+/// Application entry point
+import std.getopt;
+import std.stdio;
+
 /// Workaround for issue likely related to dub #225,
 /// expects a main() function and invokes it after unittesting.
 version (unittest) void main () { } else:
 
 /// Application entry point
-private int main ()
+private int main (string[] args)
 {
-    //  To do
-    //  Parse Command Line
-    //  Make Transaction
-    //  Send Transaction
+    CommandLine cmdLine;
+    try
+    {
+        auto help = parseCommandLine(cmdLine, args);
+        if (help.helpWanted)
+        {
+            defaultGetoptPrinter("The Agora cli", help.options);
+            return 0;
+        }
+    }
+    catch (Exception ex)
+    {
+        writefln("Error parsing command-line arguments '%(%s %)': %s", args,
+            ex.message);
+    }
 
-    return 0;
+    try
+    {
+        //  To do
+        //  Make Transaction
+        //  Send Transaction
+        return 0;
+    }
+    catch (Exception ex)
+    {
+        writefln("Failed to processing. Error: %s", ex.message);
+        return 1;
+    }
 }
